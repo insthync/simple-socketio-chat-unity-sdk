c768c632926a53098ca3c9db7e00dbbbc8e39acb
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-03-31 14:59:15 +0700

    Change instance initializing

--- Scripts/SocketIOChatManager.cs
+++ Scripts/SocketIOChatManager.cs
@@ -14,8 +14,6 @@ namespace SimpleSocketIOChatSDK
         {
             get
             {
-                if (!instance)
-                    new GameObject("_SocketIOChatManager").AddComponent<SocketIOChatManager>();
                 return instance;
             }
         }
